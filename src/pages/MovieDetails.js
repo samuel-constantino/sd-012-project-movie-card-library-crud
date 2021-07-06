3a61b30dccb29779073891eb76e81aa0488c446d
Samuel Constantino <samuelicapuidados@gmail.com>
2021-07-06 19:14:45 -0300

    implementa requisito 4: mostrar página de detalhes

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,66 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor(props) {
+    super(props);
+    this.state = {
+      movie: {},
+      loading: true,
+    };
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    this.setMovie = this.setMovie.bind(this);
+  }
+
+  componentDidMount() {
+    this.setMovie();
+  }
+
+  async setMovie() {
+    const { match } = this.props;
+    const { id } = match.params;
+    const { getMovie } = movieAPI;
+
+    const currentMovie = await getMovie(id);
+    this.setState({
+      movie: currentMovie,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { match } = this.props;
+    const { id } = match.params;
+    const { movie, loading } = this.state;
+
+    if (loading) return <Loading>Carregando...</Loading>;
+
+    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <h2>{ `Title: ${title}` }</h2>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <Link to={ `${id}/edit` }>EDITAR</Link>
+        <Link to="/">VOLTAR</Link>
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default MovieDetails;
