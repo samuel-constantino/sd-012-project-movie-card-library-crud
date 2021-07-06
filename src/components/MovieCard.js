e6388eb73bbb0f49b8a77eeb90cb124132466e6d
Samuel Constantino <samuelicapuidados@gmail.com>
2021-07-06 18:19:46 -0300

    refatora requisito 3: diminui quantidade de detalhes prévios

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -8,28 +8,15 @@ class MovieCard extends React.Component {
     const {
       id,
       title,
-      subtitle,
       imagePath,
-      storyline,
-      rating } = movie;
+      storyline } = movie;
     return (
       <div data-testid="movie-card">
-        <div>{ title }</div>
-        <div>{ subtitle }</div>
         <img src={ imagePath } alt={ `imagem do filme ${title}` } />
-        <div>{ storyline }</div>
-        <div>{ rating }</div>
+        <h2>{ title }</h2>
+        <p>{ storyline }</p>
         <Link to={ `movies/${id}` }>VER DETALHES</Link>
       </div>
-
-// id: 1,
-// title: 'Kingsglaive',
-// subtitle: 'Final Fantasy XV',
-// storyline: "King Regis, who oversees the land of Lucis, commands his army of soldiers to protect the kingdom from the Niflheim empire's plans to steal the sacred crystal.",
-// rating: 4.5,
-// imagePath: 'images/Kingsglaive_Final_Fantasy_XV.jpg',
-// bookmarked: true,
-// genre: 'action',
     );
   }
 }
@@ -38,10 +25,8 @@ MovieCard.propTypes = {
   movie: PropTypes.shape({
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
-    subtitle: PropTypes.string.isRequired,
     imagePath: PropTypes.string.isRequired,
     storyline: PropTypes.string.isRequired,
-    rating: PropTypes.number.isRequired,
   }).isRequired,
 };
 
